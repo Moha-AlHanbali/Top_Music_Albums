b728c8338d0dc7baff194144a3879f0ac6f99226
MohammedAlHanba <>
2021-06-03 12:22:05 +0000

    added js

---
+++ credits_script.js
@@ -0,0 +1,5 @@
+
+
+var a = "abc"
+console.log(a)
+document.write("<p>&copy; Mohammed Al-Hanbali in collab with Abdallah Hamoury</p>" + "<h6 style='margin-left: 575px; display: inline;'> Powered By Javascript</h6>")
---
+++ script.js
@@ -0,0 +1,33 @@
+
+alert("Hello, Welcome to My Top Metal Albums Page")
+var visitorname = prompt("What is your name?")
+alert("Nice to meet you "+visitorname)
+var q1answer = prompt("Do you like Metal music " + visitorname + " ?")
+
+
+if (q1answer=="yes"){alert("I hope you enjoy my taste in music then!")} else{
+if (q1answer=="yup"){alert("I hope you enjoy my taste in music then!")} else{
+if (q1answer=="yeah"){alert("I hope you enjoy my taste in music then!")} else{
+if (q1answer=="YES"){alert("I hope you enjoy my taste in music then!")} else{
+if (q1answer=="YUP"){alert("I hope you enjoy my taste in music then!")} else{ 
+if (q1answer=="YEAH"){alert("I hope you enjoy my taste in music then!")} else{
+// I'll fix this mess later with an array later 
+if (q1answer=="no"){alert("Too bad, I hope I can change your mind " + visitorname + " !")} else{
+if (q1answer=="nah"){alert("Too bad, I hope I can change your mind " + visitorname + " !")} else{
+if (q1answer=="nope"){alert("Too bad, I hope I can change your mind " + visitorname + " !")} else{
+if (q1answer=="NO"){alert("Too bad, I hope I can change your mind " + visitorname + " !")} else{
+if (q1answer=="NAH"){alert("Too bad, I hope I can change your mind " + visitorname + " !")} else{
+if (q1answer=="NOPE"){alert("Too bad, I hope I can change your mind " + visitorname + " !")} else{
+  alert("Not interested in answering " + visitorname + " ?")
+
+}}}}}}}}}}}}
+alert("I need you to do one last thing for me!")
+
+confirm("Promise me you'll try one of my top Albums!")
+alert("AND!")
+alert("Enjoy your visit!♪")
+
+// var Yes=["yes", "YES","yeah","YEAH","yup","YUP"];
+// var No=["no", "NO", "nope", "NOPE", "nah", "NAH"]
+
+// if (Yes.IndexOf(q1answer)==-1){alert ("TEST TEST")}
